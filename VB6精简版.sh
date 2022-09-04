f812f547a20a6ea6f59fe135e50f5aa8eb7ca979
夜雨未稠 <88321115+oynos@users.noreply.github.com>
2022-09-04 17:40:28 +0800

    Add files via upload

---
+++ VB6精简版.sh
@@ -0,0 +1,1 @@
+env WINEPREFIX="/root/.wine" wine C:\\Program\ Files\ \(x86\)\\VB精简版\\Vb6.exe
---
+++ vb6企业版.sh
@@ -0,0 +1,1 @@
+env WINEPREFIX="/root/.wine" wine C:\\Program\ Files\ \(x86\)\\Microsoft\ Visual\ Studio\\VB98\\VB6.EXE
